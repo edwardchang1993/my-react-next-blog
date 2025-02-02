e5469d3b36dd873485d9bc43f916f90e5814e320
張玴豪 <edwardchang@zhangyihaodeMacBook-Pro.local>
2025-02-02 17:19:17 +0800

    [NEW] 初步完成 About 頁面

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,104 @@
-export default function about() {
-    return <div>About</div>
+"use client";
+
+import { useState, useEffect } from "react";
+import { styled } from "styled-components";
+import toast, { Toaster } from "react-hot-toast";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "@/sdk/firebase";
+import TiptapContentEditor from "@/components/TiptapContentEditor";
+import SubmitButton from "@/components/SubmitButton";
+import FullScreenLoading from "@/components/FullscreenLoading";
+import EditorContentPreview from "@/components/EditorContentPreview";
+import type { AboutMeDataType, AboutMeDataContentType } from "@/types/editor";
+
+const AboutMeTiptapFooter = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 24px;
+`;
+
+export default function AboutMePage() {
+  const collectionId = process.env
+    .NEXT_PUBLIC_FIRESTORE_ABOUT_ME_COLLECTION_ID as string;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isFirestoreDataLoaded, setIsFirestoreDataLoaded] =
+    useState<boolean>(false);
+  const [originAboutMe, setOriginAboutMe] =
+    useState<AboutMeDataContentType>("");
+  const [newAboutMe, setNewAboutMe] = useState<AboutMeDataContentType>("");
+
+  async function fetchAboutMeDocument() {
+    const docRef = doc(db, "about_me", collectionId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const aboutMeData = docSnap.data() as AboutMeDataType;
+
+      setOriginAboutMe(aboutMeData.about_me_content);
+      setNewAboutMe(aboutMeData.about_me_content);
+    } else {
+      console.log("[docSnap not exists]", docSnap);
+    }
+  }
+
+  async function submit() {
+    if (!newAboutMe) {
+      toast.error("內容不可為空");
+      return;
+    }
+
+    try {
+      const data = {
+        about_me_content: newAboutMe,
+        update_timestamp: new Date().getTime(),
+      };
+
+      setIsLoading(true);
+
+      const docRef = doc(db, "about_me", collectionId);
+
+      await updateDoc(docRef, data);
+
+      setOriginAboutMe(newAboutMe);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchAboutMeDocument();
+  }, []);
+
+  useEffect(() => {
+    if (newAboutMe) {
+      setIsFirestoreDataLoaded(true);
+    }
+  }, [newAboutMe]);
+
+  return (
+    <>
+      {isFirestoreDataLoaded ? (
+        <>
+          <EditorContentPreview
+            editionContent={originAboutMe}
+            hasReadMore={false}
+          />
+          <div style={{ padding: "16px 0" }} />
+          <TiptapContentEditor
+            editorContent={newAboutMe}
+            setEditorContent={setNewAboutMe}
+          />
+        </>
+      ) : (
+        <></>
+      )}
+      <AboutMeTiptapFooter>
+        <SubmitButton label="儲存" onClick={submit} />
+      </AboutMeTiptapFooter>
+      <Toaster />
+      <FullScreenLoading isLoading={isLoading} />
+    </>
+  );
 }
