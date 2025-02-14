7cfd8e795096dac676479d22a64ca0ae876d6d6e
張玴豪 <s2995510@gmail.com>
2025-02-14 17:02:43 +0800

    [NEW] 新增 Run Build 成功後自動生成 sitemap 的機制

---
+++ next-sitemap.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next-sitemap').IConfig} */
+
+const config = {
+  siteUrl: "https://edwardchang.blog/",
+  generateRobotsTxt: true, // 產生 robots.txt
+  sitemapSize: 5000, // 可選，控制 sitemap 頁數
+  exclude: [], // 可選，排除特定頁面
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "Googlebot", allow: "/" },
+    ],
+  },
+};
+
+export default config;
