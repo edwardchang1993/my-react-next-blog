373f3967b105a93835c294e3b37b8630886ebc00
張玴豪 <s2995510@gmail.com>
2025-06-17 17:04:07 +0800

    [UPGRADE] 更新 metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,32 +4,62 @@ import { Varela_Round } from "next/font/google";
 import { StyledComponentsRegistry } from "@/components";
 import { GoogleAuthProvider } from "@/context/GoogleAuthContext";
 import { LayoutWrapper } from "@/components";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Edward Chang's Blog",
   description: "A frontend engineer's personal blog.",
+  keywords: ["Edward", "frontend", "web", "engineer", "blog"],
+  authors: [
+    {
+      name: "Edward",
+    },
+  ],
+  creator: "Edward Chang",
+  publisher: "",
+  applicationName: "React Next Blog",
+  generator: "Next.js",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  referrer: "origin-when-cross-origin",
+  category: "blog",
   openGraph: {
     type: "website",
+    locale: "zh_TW",
     url: "https://edwardchang.blog/blog/",
-    title: "Edward Chang's Blog",
+    title: "Edward's Blog",
+    siteName: "Edward's Blog",
     description: "A frontend engineer's personal blog.",
     images: [
       {
         url: "https://i.meee.com.tw/mIidscN.png",
         width: 520,
         height: 520,
-        alt: "My Blog",
+        alt: "Edward's Blog",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Edward Chang's Blog",
+    title: "Edward's Blog",
     description: "A frontend engineer's personal blog.",
     images: ["https://i.meee.com.tw/mIidscN.png"],
   },
   icons: {
     icon: "https://i.meee.com.tw/mIidscN.png",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+    },
   },
 };
 
